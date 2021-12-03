62b1903881eed7562ff0bc5c3ccaec5ac1c42b6e
ZR21070043-尹文龙 <yinwenlong@zhongruigroup.com>
2021-12-03 16:46:43 +0800

    环境构建

---
+++ src/main/java/com/bestrookie/pojo/DepartmentPojo.java
@@ -0,0 +1,18 @@
+package com.bestrookie.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author bestrookie
+ * @date 2021/12/1 10:18 上午
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DepartmentPojo {
+    private String id;
+    private String name;
+    private String tel;
+}
